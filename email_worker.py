09b086b8b4bacf6190d4ee35a54079e1bbf051ab
Misha Polunin <polunin1986@gmail.com>
2026-01-10 14:32:32 +0200

    -  temporary decision part two

--- email_worker.py
+++ email_worker.py
@@ -2,6 +2,7 @@ from celery import Celery
 from celery.schedules import crontab
 from flask import render_template
 from sqlalchemy import select
+from email.mime.text import MIMEText
 
 import database
 import models
@@ -24,18 +25,20 @@ def setup_periodic_tasks(sender, **kwargs):
 def send_email(recipient, subject, new_films):
     FROM = os.environ.get("FROM_EMAIL")
     TO = recipient if isinstance(recipient, list) else [recipient]
-    SUBJECT = "Film Notification"
-    TEXT = render_template("email_template.html", new_films=new_films)
 
-    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    TEXT = render_template("digest.html", new_films=new_films)
+
+    msg = MIMEText(TEXT, "html", "utf-8")
+    msg["From"] = FROM
+    msg["To"] = ", ".join(TO)
+    msg["Subject"] = subject
 
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
         server.ehlo()
         server.starttls()
         server.login('user@gmail.com', 'xxxx xxxx xxxx xxxx')# user and password as mask
-        server.sendmail(FROM, TO, message)
+        server.sendmail(FROM, TO, msg.as_string())
         server.close()
         print('successfully sent the mail')
     except Exception as e:
@@ -50,10 +53,18 @@ def build_and_send_daily_emails():
         new_films = session.execute(stmt).scalars().all()
         if not new_films:
             return
+        # Celery works with dict, list, str, int, bool, float, None, JSON-like
+        # So we need to convert our Film objects to dict
+        # ORM does not support serialization
+        films_data = [{
+            'title': film.title,
+            'description': film.description,
+            'added_at': film.added_at.isoformat()
+        } for film in new_films]
 
         email_users = session.query(models.User).filter(models.User.email.isnot(None)).all()
 
         for user in email_users:
-            send_email.delay(user.email, "Newest Films Just Added", new_films)
+            send_email.delay(user.email, "Newest Films", films_data)
     finally:
         session.close()
